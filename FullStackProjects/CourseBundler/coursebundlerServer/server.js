114a121acb775e5405255006f160706d1d866f0a
pjswarnpath2709 <pjswarnpath@gmail.com>
2023-04-12 12:42:24 +0530

    again some testing changes

--- FullStackProjects/CourseBundler/coursebundlerServer/server.js
+++ FullStackProjects/CourseBundler/coursebundlerServer/server.js
@@ -25,7 +25,7 @@ connectDb().then(() => {
   cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
     api_key: process.env.CLOUDINARY_CLIENT_API,
-    api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+    api_secret: String(process.env.CLOUDINARY_CLIENT_SECRET),
   });
   console.log(
     "\x1b[35m",
